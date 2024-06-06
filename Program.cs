c1c4ba8cdc0f052f90724b54f0867a9e1d32a2a0
alexk <alexk@DESKTOP-QL7PNT4>
2024-06-06 14:57:02 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+namespace Week_5_Challenge_Lab_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numDict = new Dictionary<int, int>();
+
+            int[] nums = {4,1,2,1,2};
+
+            foreach(int i in nums) 
+            {
+                if (nums.Length == 1) 
+                {
+                    Console.WriteLine(i);
+                }
+                else if (!numDict.ContainsKey(i))
+                {
+                    numDict[i] = 1;
+                }
+                else 
+                {
+                    numDict[i]++;
+                }
+            ;
+            
+            }
+            foreach(int num in numDict.Keys) 
+            {
+                if (numDict[num] == 1) 
+                {
+                    Console.WriteLine(num);
+                }
+            }
+        }   
+    }
+}
